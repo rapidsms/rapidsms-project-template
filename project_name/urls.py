0f35d9587b4603aaeb7305e37cc30414858ab940
Dan Poirier <dpoirier@caktusgroup.com>
2013-03-08 10:14:19 -0500

    Add admin.autodiscover - fixes #6 Branch: release-0.12.0

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     # RapidSMS core URLs
